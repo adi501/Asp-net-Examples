f41b82aaf99104566a7c36c6c83dea3d5be714ff
Adinarayana Reddy JC <jc.adi101@gmail.com>
2022-01-13 12:29:32 +0530

    added new page Private fields

---
+++ WebApplication1/Private_Fields.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Private_Fields.aspx.cs" Inherits="WebApplication1.Private_Fields" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button id="btn1" runat="server" Text="Submit" OnClick="btn1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Private_Fields.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public partial class Private_Fields : System.Web.UI.Page
+    {
+        private static bool abc = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            { 
+            bind();
+            }
+        }
+        public void bind()
+        {
+            abc = true;
+        }
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            bool aaaa = abc;
+        }
+    }
+}
